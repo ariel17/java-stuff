825901e203bd01256c32cf0eb3fefc5dc882cf4f
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-06-04 01:57:42 -0300

    ADD: other test with javadocs.

---
+++ Thisisothertest.java
@@ -0,0 +1,29 @@
+//: object/Thisisothertest
+
+import java.util.*;
+
+/** Ohhhh yeah definition
+ * @author me
+ * @version first :)    
+*/
+public class Thisisothertest { 
+    /* code */
+
+    /** Ohhhh yeah fuck method definition
+     * @param lala
+     * @return idunoo
+     * @throws none
+    */
+    public String method() { 
+        /* code */ 
+        return "Hello world!";
+    }
+
+    public static void main (String [] args)
+    {
+        Thisisothertest a = new Thisisothertest();
+        System.out.println(a.method());
+    }
+}
+
+//:~
